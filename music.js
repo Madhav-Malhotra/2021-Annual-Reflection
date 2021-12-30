624d2405574de6282ad63cb1685b4d403b04bf60
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2021-12-30 10:13:16 -0500

    Added music controls

---
+++ music.js
@@ -0,0 +1,40 @@
+document.getElementById("home-button").addEventListener("click", () => window.location = "/index.html");
+
+const music = ["CleanWaterUponYou", "Eternity", "MemoirOfSolitude", "Motion", "WeAreSaved"];
+let currAudio;
+const musicButton = document.getElementById("music-button");
+let musicCopy = JSON.parse(JSON.stringify(music));
+
+function musicLoader() {
+  window.onclick = null;
+  window.onscroll = null;
+  let currMusic = musicCopy.length > 0 ? musicCopy : initMusicCopy();
+  console.log(currMusic);
+  const rand = Math.floor(Math.random() * currMusic.length);
+  const song = new Audio(`assets/music/${currMusic[rand]}.mp3`);
+  song.play();
+
+  currAudio = song;
+  musicButton.innerText = "Sound On";
+  musicCopy.splice(rand, 1);
+  song.addEventListener("ended", () => musicLoader());
+}
+
+function initMusicCopy() {
+  const newMusic = JSON.parse(JSON.stringify(music));
+  musicCopy = newMusic;
+  return newMusic;
+};
+
+function musicToggle(el) {
+  const state = el.innerText;
+  if (state == "Sound On") {
+    currAudio.pause();
+    el.innerText = "Sound Off";
+  } else {
+    currAudio.play();
+    el.innerText = "Sound On";
+  }
+}
+
+window.onclick = musicLoader;
